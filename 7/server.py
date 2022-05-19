07d743b7588e2dd49f0c68cb94fbfd3f66e8800f
sash00k <s.a.c.h.a.b@yandex.ru>
2022-05-19 21:11:41 +0700

    7th hw

--- 7/server.py
+++ 7/server.py
@@ -22,7 +22,8 @@ class MyServer:
     def read_file(self, path: str) -> list:
         with open(self.name + '_' + path[1:] + '.csv', 'r') as file:
             raw_info = csv.reader(file)
-            info = [{'STUDENT_ID': row[0], 'NAME': row[1]} for row in raw_info]
+            headers = next(raw_info)
+            info = [{header: row[i] for header, i in zip(headers, range(len(headers)))} for row in raw_info]
 
         return info
 
@@ -44,15 +45,19 @@ class MyServer:
                 'code': code,
                 'body': body}
 
-    def post(self, path: str, new_name: str) -> dict:
+    def post(self, path: str, new_info: list) -> dict:
+    # этот метод релизован только для случая добавления новой строки в табличку, где все остортировано по STUDENT_ID
+    # ясно, что запрос на добавление, например, оценок, работал бы иначе, ну он и принимал бы другой набор данных
         try:
             self.check_permission(path=path)
             if path not in self.routes['POST']:
                 raise FileNotFoundError
             with open(self.name + '_' + path[1:] + '.csv', 'a') as file:
+                # у нас такая простенькая база данных, что в каждой таблице ключевое значения - 'STUDENT_ID'
+                # так что поэтому оно тут так явно и прописано
                 index = int(self.read_file(path=path)[-1]['STUDENT_ID']) + 1
                 file_writer = csv.writer(file)
-                file_writer.writerow([index, new_name])
+                file_writer.writerow([index]+new_info)
             code = 200
             status = 'OK'
         except FileNotFoundError:
@@ -69,18 +74,20 @@ class MyServer:
 
 if __name__ == '__main__':
     server = MyServer(db_name='MyDatabase',
-
                       routes={'GET': {'/students', '/grades', '/schedule', '/passwords', '/payments'},
                               'POST': {'/students'}},
                       private_routes={'/passwords', '/payments'})
 
     # correct requests
     print(server.get(path='/students'))
-    print(server.post(path='/students', new_name='Бочкарев Алекcандр Дмитриевич'))
+    print(server.get(path='/grades'))
+    print(server.get(path='/schedule'))
+    print(server.post(path='/students', new_info=['Бочкарев Алекcандр Дмитриевич']))
     print()
 
     # incorrect requests
     print(server.get(path='/invalid_path'))
     print(server.get(path='/passwords'))
-    print(server.post(path='/payments', new_name='Бочкарев Алекcандр Дмитриевич'))
+    print(server.post(path='/payments', new_info=['Бочкарев Алекcандр Дмитриевич']))
+
 
